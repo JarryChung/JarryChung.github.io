b4ee51b26984a1b106f97ed12c2abff3f0225152
JarryChung <jarrychung@outlook.com>
2020-04-12 15:26:12 +0800

    [INIT] Initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+(function randomColor () {
+  const nameStyle = 'font-size: 20px; background: #1B333B; color: #F6F4F2; padding: 5px 8px; border-radius: 8px;'
+  const textStyle = 'font-size: 16px; background: #F6F4F2; color: #1B333B; padding: 5px 9px; border-radius: 8px;'
+  console.log('%c Jarry Chung ', nameStyle)
+  console.log('%c Nice to meet u ', textStyle)
+})()
+
